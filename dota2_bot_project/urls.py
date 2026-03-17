9d15645ce50c9dab23bb01285fe63f397a5f4c9a
Артур Батыршин <Artur20.06.2000@icloud.com>
2026-03-17 04:10:01 +0300

    Fix: add missing import for telegram_webhook

--- dota2_bot_project/urls.py
+++ dota2_bot_project/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from bot.webhook import telegram_webhook
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('webhook/', telegram_webhook, name='telegram_webhook'),
+    path("admin/", admin.site.urls),
+    path("webhook/", telegram_webhook, name="telegram_webhook"),
 ]
